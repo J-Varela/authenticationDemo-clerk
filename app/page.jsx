9ecf00c41a6aaf73f2849ee853960dd38478b73d
Jonathan Varela <jonvarela4@gmail.com>
2023-08-25 17:24:26 -0400

    initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ export default function Home() {
     <>
       <h1 className="text-2xl font-bold mb-5">Home</h1>
       <p className="mb-5">
-        This is a demo site for the Traversy Media Next.js & Clerk tutorial. Go
-        ahead and sign up or sign in!
+        This is a demo site for the Next.js & Clerk tutorial. Go ahead and sign
+        up or sign in!
       </p>
     </>
   );
